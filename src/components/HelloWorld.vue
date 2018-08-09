6e356b130f6bf9f5843ef1f71e675ef700fdf40b
eyea <deyesea@gmail.com>
2018-08-09 23:43:42 +0800

    add project url

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,8 @@
       <el-aside :style="{width: asideBigWidth?'200px':'50px'}">
         <!-- <el-menu :default-openeds="['1', '3']"> -->
         <el-menu>
-          <el-submenu @click="handleComponent" v-for="(item, index) in menuList" :key="index" :index="(index+1).toString()" style="text-align: left;">
+          <el-submenu v-for="(item, index) in menuList" :key="index" :index="(index+1).toString()" style="text-align: left;"
+              @click="handleComponent">
             <template slot="title"><i :class="'el-icon-'+item.iconClass"></i>{{item.title}}</template>
             <el-menu-item-group v-if="item.hasChildren">
               <el-menu-item v-for="(it,ind) in item.children" :key="it.name+ind" :index="index+'-'+ind">{{it.name}}</el-menu-item>
@@ -49,7 +50,7 @@
             <el-dropdown>
               <i class="el-icon-setting" style="margin-right: 15px"></i>
               <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>Github</el-dropdown-item>
+                <el-dropdown-item> <a href="https://github.com/sayabc/vue-admin" target="_blank">Github</a></el-dropdown-item>
                 <el-dropdown-item>Log Out</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
@@ -122,6 +123,9 @@ export default {
     handleComponent() {
       console.log('this')
       // this.$store.commit('HANDLE_CUR_COMPONENT', 'Test') // 同步更改组件
+    },
+    handleUrl(url) {
+      window.open(url)
     }
   },
   mounted() {
