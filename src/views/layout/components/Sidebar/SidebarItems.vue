269d8213c9b5542ab836ad8e0883d087bd7b7bce
eyea <deyesea@gmail.com>
2018-08-15 19:55:57 +0800

    update little

--- src/views/layout/components/Sidebar/SidebarItems.vue
+++ src/views/layout/components/Sidebar/SidebarItems.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="sidebar_item">
-
+  <div class="sidebar_item" v-if="item.children">
+    <!-- 如果该条item没有子元素 -->
+    <router-link v-if="hasOneChild(item.children)" :to="item.children[0].path">
+      {{ item.children[0].name }}
+    </router-link>
+    <!-- 该条 item 有二级菜单 -->
   </div>
 </template>
 
 <script>
 export default {
   props: ['item'],
+  methods: {
+    hasOneChild(ic) {
+      return true
+    }
+  }
 }
 </script>
 
